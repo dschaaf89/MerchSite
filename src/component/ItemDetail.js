69c6d7032febb14e2f98201d699c12b1285075ec
Mike Manchee <mikemanchee@gmail.com>
2020-11-18 15:41:21 -0800

    edit itemDetail in project
    Co-authored-by: Adam Mansell <adammansellhmb@gmail.com>
    Co-Authored-By: Joseph Nilles <jbnilles24@gmail.com>

--- src/component/ItemDetail.js
+++ src/component/ItemDetail.js
@@ -1,11 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function ItemDetails() {
+function ItemDetail(props){
+  const { item, onClickingDelete } = props;
+
   return (
     <React.Fragment>
-      <p>{}this is a test</p>
+      <h1>Item Detail</h1>
+      <h3>{item.location} - {item.names}</h3>
+      <p><em>{item.issue}</em></p>
+      <button onClick = { props.onClickingEdit }>Update Item</button>
+      <button onClick = {() => onClickingDelete(item.id) }>Close Item</button>
+      <hr/>
     </React.Fragment>
   );
 }
+
+ItemDetail.propTypes = {
+  item: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
 
 export default ItemDetails;
