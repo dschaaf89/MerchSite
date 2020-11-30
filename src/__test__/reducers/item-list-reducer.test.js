2929a1f83ffb9f85aaeac5475fe4c678b89c19af
Mike Manchee <mikemanchee@gmail.com>
2020-11-30 09:42:40 -0800

    add ADD_ITEM test to the project
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- src/__test__/reducers/item-list-reducer.test.js
+++ src/__test__/reducers/item-list-reducer.test.js
@@ -1,8 +1,34 @@
 import itemListReducer from '../../reducers/item-list-reducer';
 
 describe('itemListReducer', () => {
-
+  let action;
+  const itemData = {
+    Name: 'Cat',
+    Description: 'An orange cat',
+    Quantity: 1,
+    Id: 1
+  };
+  
   test('Should return default state if no action type is recognized', () => {
     expect(itemListReducer({}, {type: null})).toEqual({});
   });
-})
+  
+  test('should successfully add new item data to itemCatalog', () => {
+  const { name, description, quantity, id } = itemData;
+  action = {
+    type: 'ADD_ITEM',
+    Name: name,
+    Description: description,
+    Quantity: quantity,
+    Id: id
+  };
+    expect(itemListReducer({},action)).toEqual({
+      [id]:{
+        Name: name,
+        Description: description,
+        Quantity: quantity,
+        Id: id        
+      }
+    });
+  });
+});
